affed0e3216f32e99aa5a307908c70fd9d3a06fb
to-MrGreen <samiulhaquetanu@gmail.com>
2021-09-10 17:51:32 +0600

    Up to 25% basic

--- index2.js
+++ index2.js
@@ -60,4 +60,89 @@ var product = 2.0 * 2.5;
 
 //----------Divide One Decimal by Another with JavaScript--------
 
+var quotient = 4.4 / 2.0;
+
+//-----Finding a Remainder in JavaScript------------
+var remainder;
+remainder = 11 % 3;
+
+
+//------Compound Assignment With Augmented Addition----------
+var a = 3;
+var b = 17;
+var c = 12;
+
+a+= 12;
+b+= 9;
+c+=7;
+
+//-------Compound Assignment With Augmented Subtraction--------
+var a = 11;
+var b = 9;
+var c = 3;
+
+a-= 6;
+b-= 15;
+c-= 1;
+//----Compound Assignment With Augmented Multiplication-----
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a*= 5;
+b*=3;
+c*=10;
+
+//---Compound Assignment With Augmented Division------
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+a/=12;
+b/= 4;
+c /= 11;
+
+//-----Declare String Variables------
+var myFirstName = "Md. Samiul";
+var myLastName = "Haque";
+
+//---Escaping Literal Quotes in Strings---------
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+//ans : I am a "double quoted" string inside "double quotes".
+
+//----Quoting Strings with Single Quotes-------
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+/*
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
+*/
+
+//--------Escape Sequences in Strings----
+var myStr="FirstLine\n\t\\SecondLine\nThirdLine";
+
+//-----Concatenating Strings with Plus Operator----------
+var myStr="This is the start. "+"This is the end."; 
+
+
+//-------------Concatenating Strings with the Plus Equals Operator-------
+
+
+var myStr="This is the first sentence. ";
+myStr+="This is the second sentence.";
+
+//---Constructing Strings with Variables-------
+
+var myName="Samiul Haque";
+var myStr="My name is"+myName+"and I am well!";
+
+
 
