cc59f8dfb987cfc3ab066c4591d4d701b5b20b9e
to-MrGreen <samiulhaquetanu@gmail.com>
2021-09-11 03:52:15 +0600

    72%

--- function1.js
+++ function1.js
@@ -290,5 +290,158 @@ else{
 }
 testSize(7);
 
-//------Golf Code------
+//-----Selecting from Many Options with Switch Statements-------
+function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
 
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+switchOfStuff(1);
+//--------------Multiple Identical Options in Switch Statements--------------------
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);
+
+//-----Replacing If Else Chains with Switch-----
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+chainToSwitch(7);
+
+//--Returning Boolean Values from Functions--
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
+
+//-----Return Early Pattern for Functions--------
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+
+  if (a < 0 || b < 0) return undefined;
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2,2);
+
+//-----Counting Cards-----------
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+  var msg = '';
+
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count = count;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  if (count === 5) msg = "5 Bet";
+  else if (count === 0) msg = "0 Hold";
+  else if (count === -5) msg = "-5 Hold";
+  else if (count === -1) msg = "-1 Hold";
+  else if (count === 1) msg = "1 Bet";
+
+
+  return msg;
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
