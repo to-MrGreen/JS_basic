c40cccb409884b4f59c0d8be94a14e612c19b609
to-MrGreen <samiulhaquetanu@gmail.com>
2021-09-11 02:08:18 +0600

    on going function in JS

--- function1.js
+++ function1.js
@@ -26,4 +26,67 @@ function functionWithArgs(num1, num2){
 functionWithArgs(10,20);
 
 //----Global Scope and Function-----
+// Declare the myGlobal variable below this line
+var myGlobal =10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+//-----Local Scope and Functions
+function myTest() {
+  var loc = "foo";
+  console.log(loc);
+}
+myTest();
+//console.log(loc); // this loc can't be access cause loc is local
+
+//---Global vs. Local Scope in Functions-----
+//It is possible to have both local and global variables with the same name.
+// When you do this, the local variable takes precedence over the global variable.
+
+var someVar = "Hat";
+function myFun() {
+  var someVar = "Head";
+  return someVar;
+}
+
+//----Return a Value from a Function with Return----
+function timesFive(num){
+  return 5*num;
+}
+console.log(timesFive(2));
+
+//--Understanding Undefined Value returned from a Function--
+// Setup
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+//--Assignment with a Returned Value----
 
