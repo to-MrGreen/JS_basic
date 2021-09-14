1681030c3cc9a781175516b01dd265ee17f13a7d
to-MrGreen <samiulhaquetanu@gmail.com>
2021-09-15 01:06:16 +0600

    10%

--- ES6/index1.js
+++ ES6/index1.js
@@ -55,3 +55,58 @@ function printManyTimes(str) {
 }
 printManyTimes("freeCodeCamp");
 //----------Mutate an Array Declared with const---------
+const s = [5, 7, 2];
+function editInPlace(n) {
+  // Only change code below this line
+  
+  // Using s = [2, 5, 7] would be invalid
+  "use strict";
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+  // Only change code above this line
+}
+editInPlace();
+
+//-----Prevent Object Mutation------
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+//-------Use Arrow Functions to Write Concise Anonymous Functions--------------
+const magic = () => new Date();
+
+//---------Write Arrow Functions with Parameters------------
+
+const myConcat =(arr1, arr2)=>arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+//------Use the Rest Parameter with Function Parameters---
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+}
+
+//---------Use the Spread Operator to Evaluate Arrays In-Place---------
+//ex
+//Copy all contents of arr1 into another array arr2 using the spread operator.
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+ arr2 =[...arr1];  // Change this line
+
+console.log(arr2);
