852709b46127950517dbf00e7801ecf4267242b8
to-MrGreen <samiulhaquetanu@gmail.com>
2021-09-16 02:40:11 +0600

    es6

--- ES6/index2.js
+++ ES6/index2.js
@@ -66,3 +66,29 @@ let a = 8, b = 6;
  [a, b] = [b, a];
 
 //----Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements---
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a,b, ...arr] = list; // Change this line
+  // Only change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr);
+
+//--Use Destructuring Assignment to Pass an Object as a Function's Parameters------
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({ max, min }) => (max + min) / 2.0;
+console.log(half);
+// Only change code above this line
+
+//---Create Strings using Template Literals-----
