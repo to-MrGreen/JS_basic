9a15f467014d4d1295468cbccfea7e12890a870f
to-MrGreen <samiulhaquetanu@gmail.com>
2021-09-19 23:58:33 +0600

    Done ES6 idea

--- ES6/index2.js
+++ ES6/index2.js
@@ -212,3 +212,78 @@ const lowercaseString = (string) => {
 export{uppercaseString,lowercaseString};
 
 //------------Reuse JavaScript Code Using import---------------
+import {uppercaseString, lowercaseString} from './string_functions.js';
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+//-------Use * to Import Everything from a File----------
+import * as stringFunctions from "./string_functions.js";
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+//-----Create an Export Fallback with export default--------
+
+export default function subtract(x, y) {
+  return x - y;
+}
+//----Import a Default Export------
+import subtract from "./math_functions.js";
+// Only change code above this line
+
+subtract(7,4);
+
+//-------Create a JavaScript Promise------
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
+
+//----Complete a Promise with resolve and reject---------
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {	
+    reject("Data not received");
+  }
+});
+
+//-----Handle a Fulfilled Promise with then-------------
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+	
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {	
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+//-------Handle a Rejected Promise with catch-------
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+ makeServerRequest.catch(error => {
+  console.log(error);
+});
