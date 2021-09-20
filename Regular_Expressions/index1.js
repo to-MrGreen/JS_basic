29db37298fe8c59e0549ef85a55cd06ac898cd8d
to-MrGreen <samiulhaquetanu@gmail.com>
2021-09-20 20:53:04 +0600

    counting

--- Regular_Expressions/index1.js
+++ Regular_Expressions/index1.js
@@ -18,3 +18,26 @@ let myRegex = /Hello/;
 let result =myRegex.test(myString); 
 
 //---Match Literal Strings--------
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+
+//-------Match a Literal String with Different Possibilities--------
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; 
+let result = petRegex.test(petString);
+
+//--------Ignore Case While Matching----------
+
+/*
+Up until now, you've looked at regexes to do literal matches of strings. But sometimes, you might want to also match case differences.
+
+Case (or sometimes letter case) is the difference between uppercase letters and lowercase letters. Examples of uppercase are A, B, and C. Examples of lowercase are a, b, and c.
+
+You can match both cases using what is called a flag. There are other flags but here you'll focus on the flag that ignores case - the i flag. You can use it by appending it to the regex. An example of using this flag is /ignorecase/i. This regex can match the strings ignorecase, igNoreCase, and IgnoreCase.
+*/
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+//------Extract Matches-------
