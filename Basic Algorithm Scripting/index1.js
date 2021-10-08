4a673bdd22f8dcc3a73d34769e2d5f425734b741
to-MrGreen <samiulhaquetanu@gmail.com>
2021-10-09 00:30:41 +0600

    done

--- Basic Algorithm Scripting/index1.js
+++ Basic Algorithm Scripting/index1.js
@@ -279,3 +279,35 @@ Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has
 function getIndexToIns(arr, num) {
   return arr.filter(val => num > val).length;
 }
+
+//-----Mutations------
+/* 
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+*/
+function mutation(arr) {
+  return arr[1]
+    .toLowerCase()
+    .split("")
+    .every(function(letter) {
+      return arr[0].toLowerCase().indexOf(letter) != -1;
+    });
+}
+
+//---Chunky Monkey------
+/*
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+*/
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
