59de0e8e7a5494b657e2aea154beff78ad39ad36
to-MrGreen <samiulhaquetanu@gmail.com>
2021-10-08 23:24:46 +0600

    >>>>

--- Basic Algorithm Scripting/index1.js
+++ Basic Algorithm Scripting/index1.js
@@ -244,3 +244,27 @@ function frankenSplice(arr1, arr2, n) {
 }
 
 frankenSplice([1, 2, 3], [4, 5], 1);
+
+//---------Falsy Bouncer------ 
+/*
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+*/
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
+
+//or
+function bouncer(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+//------------
+
